74d813d6d6dd843f6069acc5628438433791217f
Manuelito-V <emanuel121012@gmail.com>
2024-08-22 10:27:04 -0600

    Clase if

---
+++ clase3/app.js
@@ -0,0 +1,32 @@
+/*let numero = 2+2;
+console.log(numero);
+let resta = 5-2;
+console.log(resta);
+let mult = 5*5;
+console.log(mult);
+//Operadores relacionales
+let resultado = 10>5;
+console.log(resultado);
+console.log(typeof resultado);
+let resultado2 = 10==5;
+console.log(resultado2);
+console.log(typeof resultado2);
+
+let dif = 10 !=5;
+console.log(dif);
+console.log(typeof dif);
+*/
+//Compuertas Lógicas
+//AND && y OR 
+// 1 1 1
+/*let and = 5>2 && 6==6;
+console.log(and);*/
+
+//if
+let nomMateria = prompt("En que materia estamos? ");
+
+if(nomMateria === "web2"){
+    console.log("Correcto");
+} else {
+    console.log("Materia Erronea");
+}
---
+++ clase3/if.js
@@ -0,0 +1,7 @@
+let num = prompt("Dime un número del 1 al 10: ");
+
+if(num <=10 && num >=0){
+    console.log("Correcto");
+} else {
+    console.log("Elije otro número");
+}
